e99d60084625c37e2d63378fe4da2e6f4cc1732e
Spencer Cambern <cambern134@gmail.com>
2020-02-23 20:53:56 +0000

    Done.

--- spec/my_first_hash_spec.rb
+++ spec/my_first_hash_spec.rb
@@ -1,43 +1,99 @@
-require_relative 'spec_helper'
-require_relative '../my_first_hash.rb'
+def new_hash
 
-
-context "Challenge I: Instantiating Hashes" do
-  describe "#my_hash" do
-    it "uses the literal constructor to create a hash that contains key/value pairs" do 
-      expect(my_hash).to be_a(Hash)
-      expect(my_hash.keys.count).to_not eq(0)
-    end
-  end
+	test_hash = Hash.new
 end
 
-context "Challenge II: Hash with Data" do 
-  describe "#shipping_manifest" do 
-    it "returns a hash with key/value pairs describing old-timey items" do 
-      expect(shipping_manifest.keys).to match_array(["whale bone corsets", "oil paintings", "porcelain vases"])
-      expect(shipping_manifest.values).to match_array([2, 5, 3])
-    end
-  end
+def actor
+
+	actor = {name: "Dwayne The Rock Johnson"}
 end
 
-context "Challenge III: Retrieving Data" do 
-  describe "#retrieval" do 
-    it "operates on the shipping_manifest hash to return the value of the 'oil paintings' key" do 
-      expect(retrieval).to eq(3)
-    end
-  end
+def monopoly
+	monopoly = {}
+	monopoly = {
+		:railroads=>{}
+	}
+
 end
 
-context "Challenge IV: Adding Data" do 
-  describe "#adding" do 
-    it "operates on the shipping_manifest hash to add a key/value pair" do 
-      expect(adding).to eq({
-                      "whale bone corsets" => 5, 
-                      "porcelain vases" => 2, 
-                      "oil paintings" => 3, 
-                      "muskets" => 2,
-                      "gun powder" => 4
-                      })
-    end
-  end
+def monopoly_with_second_tier
+	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+
+	monopoly = {
+		:railroads=>
+		{
+		:pieces=>4,
+		:names=>{},
+		:rent_in_dollars=>{}
+		}
+	}
+
+end
+
+def monopoly_with_third_tier
+	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+
+end
+	monopoly = {
+		:railroads=>
+		{
+		:pieces=>4,
+		:names=>
+		{
+			:reading_railroad=>{},
+			:pennsylvania_railroad=>{},
+			:b_and_o_railroad=>{},
+			:shortline=>{}
+		},
+		:rent_in_dollars=>
+		{
+			:one_piece_owned=>25,
+			:two_pieces_owned=>50,
+			:three_pieces_owned=>100,
+			:four_pieces_owned=>200
+		}
+		}
+	}
+
+def monopoly_with_fourth_tier
+	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
+
+end
+
+def monopoly_with_fourth_tier
+	monopoly = {
+		:railroads=>
+		{
+		:pieces=>4,
+		:names=>
+		{
+			:reading_railroad=>
+			{
+				"mortgage_value"=>"$100"
+			},
+			:pennsylvania_railroad=>
+			{
+				"mortgage_value"=>"$200"
+			},
+			:b_and_o_railroad=>
+			{
+				"mortgage_value"=>"$400"
+			},
+			:shortline=>
+			{
+				"mortgage_value"=>"$800"
+			}
+		},
+		:rent_in_dollars=>
+		{
+			:one_piece_owned=>25,
+			:two_pieces_owned=>50,
+			:three_pieces_owned=>100,
+			:four_pieces_owned=>200
+		}
+		}
+	}
+
+
+
 end
